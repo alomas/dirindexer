05e93b9fed4aa194f728332c367b68d18a514ef1
Adrian <blackhatadrian@gmail.com>
2020-09-12 20:04:43 -0600

    Testing dirent.

--- dirindexer.cpp
+++ dirindexer.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <dirent.h>
 
-int main() {
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    struct dirent *entry;
+    DIR *dir;
+    const char *rootdir;
+
     std::cout << "DirIndexer v0.01α" << std::endl;
+    if (argc > 1)
+    {
+        rootdir = argv[1];
+    }
+    dir = opendir(rootdir);
+    if (dir == NULL)
+    {
+        perror("Error opening directory.");
+        exit(1);
+    }
+    while ((entry = readdir(dir)))
+    {
+        cout << entry->d_name << endl;
+    }
+
     return 0;
 }
