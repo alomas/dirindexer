56b5fdd4ae9a6098ffc529229fba44be7cf7029d
alomas <alomas@fanatics.com>
2020-09-14 18:22:09 -0600

    Added a map object to track files.

--- dirindexer.cpp
+++ dirindexer.cpp
@@ -2,10 +2,12 @@
 #include <dirent.h>
 #include <sstream>
 #include <cstring>
+#include <map>
+#include <iterator>
 
 using namespace std;
 
-int getDirectory(const char *rootdir, int depth)
+int getDirectory(const char *rootdir, int depth, std::map<string, string> *filemap)
 {
     struct dirent *entry;
     DIR *dir;
@@ -37,10 +39,16 @@ int getDirectory(const char *rootdir, int depth)
                     oss << entry->d_name;
 
                     cout <<  entry->d_name << endl;
-                    getDirectory(oss.str().c_str(), depth);
+                    getDirectory(oss.str().c_str(), depth, filemap);
                 }
                 else
                 {
+                    ostringstream oss;
+                    oss << rootdir;
+                    if (strcmp(rootdir, "/"))
+                        oss << "/";
+                    oss << entry->d_name;
+                    filemap->insert(std::make_pair(oss.str(), entry->d_name));
                     for (int f=0; f<(depth+1); f++)
                         cout << ".\t";
                     cout << entry->d_name << endl;
@@ -54,13 +62,15 @@ int main(int argc, char *argv[]) {
     struct dirent *entry;
     DIR *dir;
     const char *rootdir;
+    std::map<std::string, std::string> *filemap;
+    filemap = new std::map<string, string>;
 
     std::cout << "DirIndexer v0.01α" << std::endl;
     if (argc > 1)
     {
         rootdir = argv[1];
     }
-    getDirectory(rootdir, 0);
-
+    getDirectory(rootdir, 0, filemap);
+    cout << "Number of elements: " << filemap->size() << endl;
     return 0;
 }
